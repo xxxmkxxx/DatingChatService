447487a5b11d17741482a4c3f974e0b5821124f6
xxxmkxxx <maks88648@gmail.com>
2024-12-13 21:43:11 +0400

    refactor: remove unused methods

--- src/main/java/com/dating/chat/controller/MessageController.java
+++ src/main/java/com/dating/chat/controller/MessageController.java
@@ -4,12 +4,8 @@ import com.dating.chat.data.MessageData;
 import com.dating.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -17,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MessageController {
     public static final String MESSAGES_REQUEST = "/{dialog}";
-    public static final String CREATE_MESSAGE_REQUEST = "/{dialog}";
+    public static final String CREATE_MESSAGE_REQUEST = "/";
     private final MessageService messageService;
 
     @GetMapping(MESSAGES_REQUEST)
@@ -26,13 +22,7 @@ public class MessageController {
     }
 
     @PostMapping(CREATE_MESSAGE_REQUEST)
-    public ResponseEntity<String> createMessage(@PathVariable String dialog, @RequestBody MessageData data) {
-        return ResponseEntity.ok(messageService.createMessage(dialog, data));
-    }
-
-    @MessageMapping("/chat/dialog/{dialog}")
-    @SendTo("/topic/dialog/{dialog}/messages")
-    public String sendMessage(@DestinationVariable String dialog, String message, Principal principal) {
-        return message;
+    public ResponseEntity<String> createMessage(@RequestBody MessageData data) {
+        return ResponseEntity.ok(messageService.createMessage(data));
     }
 }
