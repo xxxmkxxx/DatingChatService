4c9c9413e6075358f6be10e0f4071d2a7ec9e585
xxxmkxxx <maks88648@gmail.com>
2024-12-15 16:56:34 +0400

    feat: add errors loging

--- src/main/java/com/dating/chat/controller/GlobalExceptionHandler.java
+++ src/main/java/com/dating/chat/controller/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.dating.chat.controller;
 
 import com.dating.chat.common.exception.DialogNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Arrays;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(DialogNotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(DialogNotFoundException exception) {
@@ -15,6 +19,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception exception) {
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Непонятная ошибка на стороне сервера!");
     }
 }
